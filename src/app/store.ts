90927c966b0ac245c0cd38d6571d1307a060eaf0
Aida-Kaiyrkulova <aida-kaiyr@yandex.ru>
2024-11-13 21:09:27 +0600

    Add Header component

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { searchReducer } from '../containers/SearchBar/searchSlice.ts';
 
 
 export const store = configureStore({
     reducer: {
+      search: searchReducer,
     },
 });
 
